ea52c604f959b78eda4a95ad6974976f3b09c1ed
veltrup <veltrup@sitepark.com>
2022-06-09 13:50:09 +0200

    refactor: move println to method

--- src/main/java/com/sitepark/release/Main.java
+++ src/main/java/com/sitepark/release/Main.java
@@ -2,7 +2,13 @@ package com.sitepark.release;
 
 @SuppressWarnings("PMD")
 public class Main {
+
 	public static void main(String[] args) {
+		Main main = new Main();
+		main.hello();
+	}
+
+	private void hello() {
 		System.out.println("Hello");
 	}
 }
