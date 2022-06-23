af82e6f0e239ed49e21bd1f4c3ad1e823eca1502
veltrup <veltrup@sitepark.com>
2022-06-23 16:37:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Project to tests maven releases with github actions.
---
+++ src/main/java/com/sitepark/release/Main.java
@@ -0,0 +1,37 @@
+package com.sitepark.release;
+
+/**
+ * Commandline Interface to greet someone
+ */
+@SuppressWarnings("PMD")
+public class Main {
+
+	/**
+	 * main
+	 */
+	public static void main(String[] args) {
+		Main main = new Main();
+		if (args.length > 0) {
+			main.hello(args);
+		} else {
+			main.usage();
+		}
+	}
+
+	/**
+	 * Print given Names
+	 */
+	private void hello(String... names) {
+		for (String name : names) {
+			System.out.println("Hello " + name);
+		}
+	}
+
+	/**
+	 * Print usage blub
+	 */
+	private void usage() {
+		System.out.println("greet user");
+		System.out.println("usage: main [name]");
+	}
+}
