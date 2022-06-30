6b4c421fc9708f00506b60aea7131d16acc2b139
veltrup <veltrup@sitepark.com>
2022-06-30 16:30:00 +0200

    add test

---
+++ src/test/java/com/sitepark/release/MainTest.java
@@ -0,0 +1,14 @@
+package com.sitepark.release;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void test() {
+		Main.main(new String[] {"Peter"});
+	}
+
+}
