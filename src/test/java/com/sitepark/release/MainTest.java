fa1b2bcd3669559f2fad25d3e230d6cdb1424d76
veltrup <veltrup@sitepark.com>
2024-02-08 08:31:17 +0100

    style: format with google java style guide

--- src/test/java/com/sitepark/release/MainTest.java
+++ src/test/java/com/sitepark/release/MainTest.java
@@ -4,34 +4,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-	private final PrintStream originalOut = System.out;
-	private final PrintStream originalErr = System.err;
+  private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+  private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+  private final PrintStream originalOut = System.out;
+  private final PrintStream originalErr = System.err;
 
-	@BeforeEach
-	public void setUpStreams() {
-		System.setOut(new PrintStream(outContent));
-		System.setErr(new PrintStream(errContent));
-	}
+  @BeforeEach
+  public void setUpStreams() {
+    System.setOut(new PrintStream(outContent));
+    System.setErr(new PrintStream(errContent));
+  }
 
-	@AfterEach
-	public void restoreStreams() {
-		System.setOut(originalOut);
-		System.setErr(originalErr);
-	}
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+    System.setErr(originalErr);
+  }
 
-	@Test
-	void test() {
-		Main.main(new String[] { "Peter" });
-		assertEquals("Hello Peter\n", this.outContent.toString(), "wrong output");
-	}
-
+  @Test
+  void test() {
+    Main.main(new String[] {"Peter"});
+    assertEquals("Hello Peter\n", this.outContent.toString(), "wrong output");
+  }
 }
